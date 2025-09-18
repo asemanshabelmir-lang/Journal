9bc61811cb319cc3fe755d88b5301330e0d5d952
asemanshabelmir-lang <asemanshabelmir@gmail.com>
2025-09-18 22:21:43 +0330

    Update select.tsx

--- src/fragment/components/select.tsx
+++ src/fragment/components/select.tsx
@@ -88,7 +88,7 @@ export const selectMeta: CodeComponentMeta<SelectType> = {
     },
     options: {
       type: "array",
-              defaultValue: [
+      defaultValue: [
           {
             label: "Option 1",
             value: "option1",
@@ -118,9 +118,9 @@ export const selectMeta: CodeComponentMeta<SelectType> = {
           },
         },
 
-        exprHint:
-          'An array of items, like `["Option1", "Option2"]`, or an array of objects with `value`, `label`, or `disabled`, like `[{value: "usa", label: "United States"}, {value: "bra", label: "Brazil"}]`',
       },
+        description:
+    'آرایه‌ای از مقادیر، مانند ["Option1","Option2"] یا آرایه‌ای از آبجکت‌ها مثل [{value:"usa",label:"United States"}]',
     },
     disabled: "boolean",
     name: {
